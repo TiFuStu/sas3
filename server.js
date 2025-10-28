04c3822068c271544325182b69986ab48274bd4d
root <root@KO0031301N.lsv.intra>
2025-10-28 09:18:01 +0100

    delete Listening only from localhost

--- server.js
+++ server.js
@@ -167,6 +167,6 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server listening on http://localhost:${PORT}`);
+app.listen(PORT, `0.0.0.0`, () => {
+  console.log(`Server listening on http://0.0.0.0:${PORT}`);
 });
