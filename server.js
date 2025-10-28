947581b68283b6e5b836a44b29ad311e7978a8e1
extranet <webmaster@lbm.rlp.de>
2025-10-28 15:00:03 +0100

    Update

--- server.js
+++ server.js
@@ -127,6 +127,6 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-app.listen(PORT, `0.0.0.0`, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server listening on http://0.0.0.0:${PORT}`);
 });
